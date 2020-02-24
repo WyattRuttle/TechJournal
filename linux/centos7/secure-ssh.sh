fe39dfbdb43ab0bfc30be9589f0f96ce37eae1ec
WyattRuttle <54773930+WyattRuttle@users.noreply.github.com>
2020-02-23 19:31:19 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,2 +1,20 @@
-#secure-ssh.sh
-#removes roots ability to ssh in
+#Wyatt Ruttle
+# Removes root ability to ssh in
+if (( $EUID != 0 )); then 
+	echo "Run with sudo privileges"
+	exit
+fi
+if [ ! -z "$1" ]; then
+	echo "Creating user $1"
+	useradd -m -d /home/$1 -s /bin/bash $1
+	mkdir /home/$1/.ssh
+	cd /home/$1/.ssh
+	wget -O authorized_keys 
+	chmod 700 /home/$1/.ssh
+	chmod 600 /home/$1/.ssh/authorized_keys
+	chown -R $1:$1 /home/$1/
+	sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+else
+	echo "Needs argument" 
+	exit
+fi 
